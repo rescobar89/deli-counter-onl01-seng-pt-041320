ff7e3a31262623f39bd6806123d24d935eed6d83
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 04:43:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,12 @@ def line(katz_deli)
   end
 end
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+    
+  else
+    puts "Currently serving #{katz_deli.first}"
+    katz_deli.shift
+  end
+end
