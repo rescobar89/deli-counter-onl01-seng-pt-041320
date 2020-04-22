c2446decdb819c1101e0858563b64c4cb2311ab4
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 04:50:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = []
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line." 
+end
+
+def line(katz_deli)
+  if katz_deli.any? == false
+    puts "The line is currently empty."
+  else
+    serving = []
+    katz_deli.each_with_index {|name, index| serving << "#{index+1}. #{name}"}
+    puts "The line is currently: " << serving.join(" ")
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+    
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
