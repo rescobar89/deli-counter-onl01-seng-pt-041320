81d793b6397505bca0c3a4421b89facab6d0da0c
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 04:44:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line" 
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line." 
 end
 
 def line(katz_deli)
